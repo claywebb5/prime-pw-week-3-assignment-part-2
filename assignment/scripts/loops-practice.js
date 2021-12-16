6b6a846a8b98aa968f54a6550bdbfbc0dd8e7d9d
Clay Webb <clay_webb@outlook.com>
2021-12-15 19:18:27 -0600

    Step 1.c. Completed, also edited structure of the answers in 1.a.-1.c.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -17,7 +17,7 @@ console.log('count from 0 to 5');
 
 for (let i=0; i<6; i++){
   console.log(i);
-} // - I changed the i<.. from i<4 in the example to i<6 in my for loop.
+} // - I changed the i<.. value to i<6 in my for loop instead of i<4 like in the example.
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
@@ -25,13 +25,18 @@ console.log('count from 3 to 5');
 
 for (let i=3; i<6; i++){
   console.log(i);
-} // - I changed the value of i to equal 3 instead of 0 like in the example, I also changed the i<.. value from i<4 in the example to i<6 in my for loop.
+} // - I changed the value of i to equal 3 instead of 0 like in the example, I also changed the i<.. value to i<6 in my for loop instead of i<4 like in the example.
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
-
+for (let i=2; i<11; i++){
+  if (i%2 === 0){
+    console.log(i);
+  }
+} // I first changed the value of i to equal 2 instead of 0 like in the example, then I changed the i<.. value to i<11 in my for loop instead of i<4 like in the example, I also
+// added the if statement in order to only console.log the even numbers.
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
